668f7d6385f557dc3bd9dae44cdc21c45ea98f18
nikhil321vijayan <nikhil321vijayan@gmail.com>
2020-05-11 13:56:38 -0400

    Error Handling

---
+++ springbootwebapp/src/main/java/org/nikhil/springbootwebapp/controller/ErrorController.java
@@ -0,0 +1,22 @@
+package org.nikhil.springbootwebapp.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("error")
+public class ErrorController {
+	
+	@ExceptionHandler(Exception.class)
+	public ModelAndView handlerError(HttpServletRequest request, HttpServletResponse response, Exception ex) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("exception",ex.getStackTrace());
+		mv.addObject("url",request.getRequestURL());
+		mv.setViewName("error");
+		return mv;
+	}
+
+}
---
+++ springbootwebapp/src/main/webapp/WEB-INF/jsp/error.jsp
@@ -0,0 +1,6 @@
+<%@ include file="common/header.jspf" %>
+<%@ include file="common/navigation.jspf" %>
+<div class="container">
+	Exception!!! Contact me in me through me!!
+</div>
+<%@ include file="common/footer.jspf" %>
